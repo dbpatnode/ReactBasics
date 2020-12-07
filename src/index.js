27475afbec875efc391d1e47b677513d90aa38e9
dbpatnodee <dbpatnode@hotmail.com>
2020-12-07 12:07:17 -0800

    rendering elements to the browser using JSX with interpolation and dot notation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-let city = "Seattle";
+let city = {
+  name: "Seattle",
+  country: "America",
+};
 
-ReactDOM.render(<h1> hello from {city}!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <h1>
+    {" "}
+    hello from {city.name} in {city.country}!
+  </h1>,
+  document.getElementById("root")
+);
