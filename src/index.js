13b65bd8618cac8adc00ac8caa020a7d30d7f957
dbpatnodee <dbpatnode@hotmail.com>
2020-12-07 14:19:01 -0800

    rendering components within other components

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-// througing the values of language, message and number within the parenthesis reduces the need to use props before them when being called in the return. This is called Destructuring
-function Hello({ language, message, number }) {
-  // console.log(Object.keys(props));
 
+function Lake({ name }) {
+  return <h1> {name} </h1>;
+}
+
+function App() {
   return (
     <div>
-      {/*if you have multiple elements within a return they need to be wrapped in a container (i.e. span, div or empt <>)  */}
-      <h1>Welcome to {language}!</h1>
-      <p> {message}</p>
-      <p> {number}</p>
-
-      {/* <p>we're using {Object.keys(props).length} props</p> */}
+      this is the app component, also heres the lake component being rendered:
+      <Lake name="Lake Washington" />
+      <Lake name="Lake Tahoe" />
+      <Lake name="Lake Mead" />
     </div>
   );
 }
 
 ReactDOM.render(
-  <Hello
-    language="React"
-    message="lets make something really cool!"
-    number={3}
-  />,
+  <App />,
+
   document.getElementById("root")
 );
