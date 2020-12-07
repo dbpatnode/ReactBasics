d7e668b83c03676260f18c2e25062b0baa923965
dbpatnodee <dbpatnode@hotmail.com>
2020-12-07 12:04:33 -0800

    using variable to render interpolated content with JSC using {}

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>
-    <ul>
-      <li>Hot Dogs</li>
-      <li>Hmburger </li>
-      <li>pizza</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+let city = "Seattle";
+
+ReactDOM.render(<h1> hello from {city}!</h1>, document.getElementById("root"));
