8173bd5baec4237a1a109a7c6c3968e26194747e
dbpatnodee <dbpatnode@hotmail.com>
2020-12-07 13:32:55 -0800

    using components to determine what is rendered then calling them on the DOM

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-let city = {
-  name: "Seattle",
-  country: "America",
-};
+function Hello() {
+  let language = "React";
+  return (
+    <div>
+      {/*if you have multiple elements within a return they need to be wrapped in a container (i.e. span, div or empt <>)  */}
+      <h1>Welcome to {language}!</h1>
+      <p> lets build something cool!</p>
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <h1>
-    {" "}
-    hello from {city.name} in {city.country}!
-  </h1>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Hello />, document.getElementById("root"));
