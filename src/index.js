dc021705f46bfa1af4d1601046143c3ee6cfb814
dbpatnodee <dbpatnode@hotmail.com>
2020-12-07 14:08:52 -0800

    use of props and destructuring

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
+// througing the values of language, message and number within the parenthesis reduces the need to use props before them when being called in the return. This is called Destructuring
+function Hello({ language, message, number }) {
+  // console.log(Object.keys(props));
 
-function Hello() {
-  let language = "React";
   return (
     <div>
       {/*if you have multiple elements within a return they need to be wrapped in a container (i.e. span, div or empt <>)  */}
       <h1>Welcome to {language}!</h1>
-      <p> lets build something cool!</p>
+      <p> {message}</p>
+      <p> {number}</p>
+
+      {/* <p>we're using {Object.keys(props).length} props</p> */}
     </div>
   );
 }
 
-ReactDOM.render(<Hello />, document.getElementById("root"));
+ReactDOM.render(
+  <Hello
+    language="React"
+    message="lets make something really cool!"
+    number={3}
+  />,
+  document.getElementById("root")
+);
